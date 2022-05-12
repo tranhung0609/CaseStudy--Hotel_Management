39385b5082aa4fea264de9b7ff5cbac7da63dbd5
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-05-12 08:16:48 +0700

    .

--- src/main/MainCase.java
+++ src/main/MainCase.java
@@ -1,9 +1,8 @@
 package main;
 
-import login.Login;
 import multithreadthing.MultithreadThing;
 
-public class demoTest {
+public class MainCase {
     public static void main(String[] args) {
         MultithreadThing multithreadThing = new MultithreadThing();
         multithreadThing.myMultithreadThing();
--- src/multithreadthing/MultithreadThing.java
+++ src/multithreadthing/MultithreadThing.java
@@ -14,29 +14,12 @@ public class MultithreadThing {
 
     public void myMultithreadThing() {
 
-        Timer timer = new Timer();
         Date now = new Date();
         System.out.println(ANSI_YELLOW + "\t\t\t現在の時刻は : " + now.getHours() + "時 : " + now.getMinutes() + "分");
         System.out.println("\t\t\tしばらくお待ちください!!");
-        TimerTask timerTask = new TimerTask() {
-            int count = 5;
-
-            @Override
-            public void run() {
-                if (count > 0) {
-                    for (int i = 0; i < count; i++) {
-                        System.out.print("\t\t"+ "\uD83C\uDD98︎");
-                    }
-                    System.out.println("");
-                    count--;
-                } else {
                     System.out.println("\t\t\tホテルラブホテル管理システムへようこそ" + ANSI_RESET);
                     Login login = new Login();
                     login.loginSystems();
-                    timer.cancel();
-                }
-            }
-        };
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, 2022);
         calendar.set(Calendar.MONTH, Calendar.MAY);
@@ -45,6 +28,5 @@ public class MultithreadThing {
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        timer.schedule(timerTask, 0, 500);
     }
 }
